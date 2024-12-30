b613fb577e2cdb42059a45f07e05939d9a356319
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 00:54:20 +0630

    Format Code

--- PresentationLayer/Controllers/BlogController.cs
+++ PresentationLayer/Controllers/BlogController.cs
@@ -1,81 +1,80 @@
-﻿namespace PresentationLayer.Controllers
+﻿namespace PresentationLayer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly BL_Blog _bL_Blog;
+
+    public BlogController(BL_Blog bL_Blog)
     {
-        private readonly BL_Blog _bL_Blog;
-
-        public BlogController(BL_Blog bL_Blog)
-        {
-            _bL_Blog = bL_Blog;
-        }
-        #region HttpGet
-        [HttpGet]
-        public async Task<IActionResult> GetBlog()
-        {
-            try
-            {
-                return Content(await _bL_Blog.GetBlogs());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        #endregion
-
-        #region HttpPost
-        [HttpPost]
-        public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
-        {
-            try
-            {
-                int result = await _bL_Blog.CreateBlog(requestModel);
-                return result > 0 ? Created() : BadRequest(MessageResource.SaveFail);
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        #endregion
-
-        #region HttpPatch
-
-        [HttpPatch("id")]
-        public async Task<IActionResult> PatchBlog([FromBody] BlogRequestModel requestModel, int id)
-        {
-            try
-            {
-                int result = await _bL_Blog.PatchBlog(requestModel, id);
-                return result > 0 ? Updated() : BadRequest(MessageResource.SaveFail);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        #endregion
-
-        #region HttpDelete
-
-        [HttpDelete]
-        public async Task<IActionResult> DeleteBlog(int id)
-        {
-            try
-            {
-                int result = await _bL_Blog.DeleteBlog(id);
-                return result > 0 ? Deleted() : BadRequest(MessageResource.DeleteFail);
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-        #endregion
+        _bL_Blog = bL_Blog;
     }
+    #region HttpGet
+    [HttpGet]
+    public async Task<IActionResult> GetBlog()
+    {
+        try
+        {
+            return Content(await _bL_Blog.GetBlogs());
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
+
+    #region HttpPost
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
+    {
+        try
+        {
+            int result = await _bL_Blog.CreateBlog(requestModel);
+            return result > 0 ? Created() : BadRequest(MessageResource.SaveFail);
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region HttpPatch
+
+    [HttpPatch("id")]
+    public async Task<IActionResult> PatchBlog([FromBody] BlogRequestModel requestModel, int id)
+    {
+        try
+        {
+            int result = await _bL_Blog.PatchBlog(requestModel, id);
+            return result > 0 ? Updated() : BadRequest(MessageResource.SaveFail);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
+
+    #region HttpDelete
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteBlog(int id)
+    {
+        try
+        {
+            int result = await _bL_Blog.DeleteBlog(id);
+            return result > 0 ? Deleted() : BadRequest(MessageResource.DeleteFail);
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    #endregion
 }
