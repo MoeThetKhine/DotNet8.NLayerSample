2262d51da292f48388ee490a9673709630c1eeea
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 00:38:47 +0630

    Format Code

--- DataAccess/Services/DA_Blog.cs
+++ DataAccess/Services/DA_Blog.cs
@@ -1,109 +1,108 @@
-﻿namespace DataAccess.Services
+﻿namespace DataAccess.Services;
+
+public class DA_Blog
 {
-    public class DA_Blog
+    private readonly AppDbContext _appDbContext;
+
+    public DA_Blog(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
-
-        public DA_Blog(AppDbContext appDbContext)
-        {
-            _appDbContext = appDbContext;
-        }
-        #region GetBlogs
-
-        public async Task<BlogListResponseModel> GetBlogs()
-        {
-            try
-            {
-                var lst = await _appDbContext.Blogs
-                    .AsNoTracking()
-                    .OrderByDescending(x => x.BlogId)
-                    .ToListAsync();
-
-                BlogListResponseModel responseModel = new()
-                {
-                    DataLst = lst.Select(x => x.Change()).ToList(),
-                };
-                return responseModel;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        #endregion
-
-        #region CreateBlog
-
-        public async Task<int> CreateBlog(BlogRequestModel requestModel)
-        {
-            try
-            {
-                await _appDbContext.Blogs.AddAsync(requestModel.Change());
-                return await _appDbContext.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        #endregion
-
-        #region PatchBlog
-
-        public async Task<int> PatchBlog(BlogRequestModel requestModel, int id)
-        {
-            try
-            {
-                var item = await _appDbContext.Blogs
-                    .AsNoTracking()
-                    .FirstOrDefaultAsync(x => x.BlogId == id)
-                    ?? throw new Exception("No Data Found");
-
-                if (!string.IsNullOrEmpty(requestModel.BlogTitle))
-                {
-                    item.BlogTitle = requestModel.BlogTitle;
-                }
-                if (!string.IsNullOrEmpty(requestModel.BlogAuthor))
-                {
-                    item.BlogAuthor = requestModel.BlogAuthor;
-                }
-                if (!string.IsNullOrEmpty(requestModel.BlogContent))
-                {
-                    item.BlogContent = requestModel.BlogContent;
-                }
-                _appDbContext.Entry(item).State = EntityState.Modified;
-                return await _appDbContext.SaveChangesAsync();
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        #endregion
-
-        #region DeleteBlog
-
-        public async Task<int> DeleteBlog(int id)
-        {
-            try
-            {
-                var item = await _appDbContext.Blogs
-                    .AsNoTracking()
-                    .FirstOrDefaultAsync(x => x.BlogId == id)
-                    ?? throw new Exception("No Data Found");
-                _appDbContext.Remove(item);
-                return await _appDbContext.SaveChangesAsync();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        #endregion
+        _appDbContext = appDbContext;
     }
+    #region GetBlogs
+
+    public async Task<BlogListResponseModel> GetBlogs()
+    {
+        try
+        {
+            var lst = await _appDbContext.Blogs
+                .AsNoTracking()
+                .OrderByDescending(x => x.BlogId)
+                .ToListAsync();
+
+            BlogListResponseModel responseModel = new()
+            {
+                DataLst = lst.Select(x => x.Change()).ToList(),
+            };
+            return responseModel;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region CreateBlog
+
+    public async Task<int> CreateBlog(BlogRequestModel requestModel)
+    {
+        try
+        {
+            await _appDbContext.Blogs.AddAsync(requestModel.Change());
+            return await _appDbContext.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region PatchBlog
+
+    public async Task<int> PatchBlog(BlogRequestModel requestModel, int id)
+    {
+        try
+        {
+            var item = await _appDbContext.Blogs
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.BlogId == id)
+                ?? throw new Exception("No Data Found");
+
+            if (!string.IsNullOrEmpty(requestModel.BlogTitle))
+            {
+                item.BlogTitle = requestModel.BlogTitle;
+            }
+            if (!string.IsNullOrEmpty(requestModel.BlogAuthor))
+            {
+                item.BlogAuthor = requestModel.BlogAuthor;
+            }
+            if (!string.IsNullOrEmpty(requestModel.BlogContent))
+            {
+                item.BlogContent = requestModel.BlogContent;
+            }
+            _appDbContext.Entry(item).State = EntityState.Modified;
+            return await _appDbContext.SaveChangesAsync();
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    #endregion
+
+    #region DeleteBlog
+
+    public async Task<int> DeleteBlog(int id)
+    {
+        try
+        {
+            var item = await _appDbContext.Blogs
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.BlogId == id)
+                ?? throw new Exception("No Data Found");
+            _appDbContext.Remove(item);
+            return await _appDbContext.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    #endregion
 }
