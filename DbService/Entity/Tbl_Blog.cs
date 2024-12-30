187c14109719e6208f9c6df541d9d5759dfb2ca1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 00:42:37 +0630

    Format Code

--- DbService/Entity/Tbl_Blog.cs
+++ DbService/Entity/Tbl_Blog.cs
@@ -1,12 +1,11 @@
-﻿namespace DbService.Entity
+﻿namespace DbService.Entity;
+
+[Table("Tbl_Blog")]
+public class Tbl_Blog
 {
-    [Table("Tbl_Blog")]
-    public class Tbl_Blog
-    {
-        [Key]
-        public long BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set;}
-        public string BlogContent { get; set;}
-    }
+    [Key]
+    public long BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set;}
+    public string BlogContent { get; set;}
 }
