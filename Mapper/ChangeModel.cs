6b9ec616c45c86016411948d225a65e38ca88d68
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 00:45:29 +0630

    Format Code

--- Mapper/ChangeModel.cs
+++ Mapper/ChangeModel.cs
@@ -1,35 +1,34 @@
-﻿namespace Mapper
+﻿namespace Mapper;
+
+public static class ChangeModel
 {
-    public static class ChangeModel
+    #region BlogModel
+
+    public static BlogModel Change(this Tbl_Blog dataModel)
     {
-        #region BlogModel
-
-        public static BlogModel Change(this Tbl_Blog dataModel)
+        return new BlogModel
         {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent,
-            };
-        }
-
-        #endregion
-
-        #region Tbl_Blog
-
-        public static Tbl_Blog Change(this BlogRequestModel requestModel)
-        {
-            return new Tbl_Blog
-            {
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-        }
-
-        #endregion
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent,
+        };
     }
-    
+
+    #endregion
+
+    #region Tbl_Blog
+
+    public static Tbl_Blog Change(this BlogRequestModel requestModel)
+    {
+        return new Tbl_Blog
+        {
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
+    }
+
+    #endregion
 }
+
