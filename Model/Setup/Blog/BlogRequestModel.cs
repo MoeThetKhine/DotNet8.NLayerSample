eba81739ff3bbe8b9f9b9465a05cec0bb153e9eb
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 00:48:29 +0630

    -

--- Model/Setup/Blog/BlogRequestModel.cs
+++ Model/Setup/Blog/BlogRequestModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace Model.Setup.Blog;
 
+#region BlogRequestModel
+
 public class BlogRequestModel
 {
     public string BlogTitle { get; set; }
     public string BlogAuthor { get; set; }
     public string BlogContent { get; set; }
 }
+
+#endregion
