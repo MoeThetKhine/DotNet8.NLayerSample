2354dc1c8df4f954cbdab941acd20d1dea2a37c5
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 01:03:16 +0630

    Format Code

--- PresentationLayer/Services/ModularService.cs
+++ PresentationLayer/Services/ModularService.cs
@@ -1,43 +1,42 @@
-﻿namespace PresentationLayer.Services
+﻿namespace PresentationLayer.Services;
+
+public static class ModularService
 {
-    public static class ModularService
+    public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddServices(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddDbContextService(builder);
-            services.AddBusinessLogicServices();
-            services.AddDataAccessServices();
-            services.AddJsonServices();
-            return services;
+        services.AddDbContextService(builder);
+        services.AddBusinessLogicServices();
+        services.AddDataAccessServices();
+        services.AddJsonServices();
+        return services;
 
-        }
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient);
-            return services;
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient);
+        return services;
 
-        }
-        private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    }
+    private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<BL_Blog>();
+        return services;
+    }
+    private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddScoped<DA_Blog>();
+        return services;
+    }
+    private static IServiceCollection AddJsonServices(this IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(opt =>
         {
-            services.AddScoped<BL_Blog>();
-            return services;
-        }
-        private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            services.AddScoped<DA_Blog>();
-            return services;
-        }
-        private static IServiceCollection AddJsonServices(this IServiceCollection services)
-        {
-            services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-            return services;
-        }
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        return services;
     }
 }
