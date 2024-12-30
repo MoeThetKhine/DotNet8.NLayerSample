77e7f8e842223a085100606c8d9f9eebd58ee95b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 00:56:20 +0630

    Format Code

--- PresentationLayer/Controllers/BaseController.cs
+++ PresentationLayer/Controllers/BaseController.cs
@@ -1,24 +1,23 @@
-﻿namespace PresentationLayer.Controllers
+﻿namespace PresentationLayer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object? obj) 
     {
-        protected IActionResult Content(object? obj) 
-        {
-            return StatusCode(200, JsonConvert.SerializeObject(obj));
-        }
-        protected IActionResult Created()
-        {
-            return StatusCode(201, MessageResource.SaveSuccess);
-        }
-        protected IActionResult Updated()
-        {
-            return StatusCode(202, MessageResource.SaveSuccess);
-        }
-        protected IActionResult Deleted()
-        {
-            return StatusCode(202, MessageResource.DeleteSuccess);
-        }
+        return StatusCode(200, JsonConvert.SerializeObject(obj));
+    }
+    protected IActionResult Created()
+    {
+        return StatusCode(201, MessageResource.SaveSuccess);
+    }
+    protected IActionResult Updated()
+    {
+        return StatusCode(202, MessageResource.SaveSuccess);
+    }
+    protected IActionResult Deleted()
+    {
+        return StatusCode(202, MessageResource.DeleteSuccess);
     }
 }
