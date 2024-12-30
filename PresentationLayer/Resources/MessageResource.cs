316aff59fa3eb444688ad797f92bcae9fb24c603
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-31 01:06:00 +0630

    Format Code

--- PresentationLayer/Resources/MessageResource.cs
+++ PresentationLayer/Resources/MessageResource.cs
@@ -1,11 +1,10 @@
-﻿namespace PresentationLayer.Resources
-{
-    public static  class MessageResource
-    {
-        public static string SaveSuccess { get; } = "Saving Successful";
-        public static string SaveFail { get; } = "Saving Fail";
-        public static string DeleteSuccess { get; } = "Deleting Successful";
-        public static string DeleteFail { get; } = "Deleting Fail";
+﻿namespace PresentationLayer.Resources;
 
-    }
+public static  class MessageResource
+{
+    public static string SaveSuccess { get; } = "Saving Successful";
+    public static string SaveFail { get; } = "Saving Fail";
+    public static string DeleteSuccess { get; } = "Deleting Successful";
+    public static string DeleteFail { get; } = "Deleting Fail";
+
 }
